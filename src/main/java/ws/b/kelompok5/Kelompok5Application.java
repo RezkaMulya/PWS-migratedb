d4f28f237f7717388c62647f4bf8f2010e657cae
RezkaMulya <rezka.mulya.ft20@mail.umy.ac.id>
2022-10-18 22:18:39 +0700

    first commit

---
+++ src/main/java/ws/b/kelompok5/Kelompok5Application.java
@@ -0,0 +1,13 @@
+package ws.b.kelompok5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kelompok5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kelompok5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/b/kelompok5/Kelompok5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.b.kelompok5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kelompok5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
